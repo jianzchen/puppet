66fb63285166211cc1ccf25960b319acf5108657
root <root@slc-puppetmaster-5357.slc01.dev.ebayc3.com>
2015-02-15 20:29:38 -0700

    class service package

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,12 +3,14 @@
  * Author: CHEN Johnson
  * Version: 1.0
 */
+$foo = [ 'one', 'two', 'three' ]
 
 node default {
+  include apache
   file {
    "/tmp/first.dat" :
     mode => 0666,
-    content => "hello, puppet\n";
+    content => "${foo[0]}\n";
   }
   exec {
    "test interval" :
@@ -24,3 +26,46 @@ node default {
     content => template("test/link.dat");
   }
 }
+
+class apache {
+  package {'httpd':
+    before => File['/etc/httpd/conf/httpd.conf'],
+  }
+  service {'httpd':
+    ensure => running,
+    enable => true,
+    require => Package['httpd'],
+    before => File['/etc/httpd/conf/httpd.conf'],
+  }
+  file { '/etc/httpd/conf/httpd.conf':
+    ensure => file,
+    owner => 'httpd',
+    group => 'httpd',
+    mode => 0666,
+    content => template('apache/httpd.conf.erb'),
+  }
+}
+
+/*
+
+# regular exp
+ if $host =~ /^www(\d+)\./ {
+      notify { "Welcome web server #$1": }
+    }
+ $packages = $operatingsystem ? {
+       /(?i-mx:ubuntu|debian)/        => 'apache2',
+       /(?i-mx:centos|fedora|redhat)/ => 'httpd',
+     }
+
+# hash indexing
+$myhash = { key       => "some value",
+                other_key => "some other value" }
+    notice( $myhash[key] )
+$main_site = { port        => { http  => 80,
+                                    https => 443 },
+                   vhost_name  => 'docs.puppetlabs.com',
+                   server_name => { mirror0 => 'warbler.example.com',
+                                    mirror1 => 'egret.example.com' }
+                 }
+    notice ( $main_site[port][https] )
+*/
